b14eca8082a5c622ed0b543ba5016e2babe7c8c0
SpatLyu <lyu.geosocial@gmail.com>
2025-06-18 00:24:22 +0800

    include `tEDM` package logo

---
+++ tEDM_logo.R
@@ -0,0 +1,39 @@
+library(showtext)
+showtext_auto(enable = TRUE)
+font_add("ShineTypewriter", regular = "./ShineTypewriter-lgwzd.ttf")
+library(hexSticker)
+library(magick)
+
+sticker(
+  subplot = "./EDM.png",
+  s_x = 1.085,
+  s_y = 0.920,
+  s_width = .6,
+  s_height = .6,
+  package = "tEDM",
+  p_family = "ShineTypewriter",
+  p_size = 18.5,
+  p_x = 1.00,
+  p_y = 1.525,
+  p_color = "#b22222",
+  dpi = 300,
+  asp = 1,
+  h_size = 2.25,
+  h_color = "#ef4c2d",
+  h_fill = "#58006d",
+  spotlight = TRUE,
+  l_x = 0.955,
+  l_y = 0.810,
+  l_width = 4.25,
+  l_height = 1.55,
+  l_alpha = 0.95,
+  white_around_sticker = F,
+  url = "https://stscl.github.io/tEDM",
+  u_color = "#b22222",
+  u_size = 5.25,
+  filename = "tEDM_logo.png"
+)
+
+image_read('./tEDM_logo.png') |> 
+  image_resize("240x278")|> 
+  image_write('./tEDM_logo.png')
---
+++ tEDM_symbols.R
@@ -0,0 +1,47 @@
+# --- Simulation Function ---
+simulate_attractor = function(f, x0, y0, z0, steps = 5000, dt = 0.01) {
+  x = numeric(steps)
+  y = numeric(steps)
+  z = numeric(steps)
+  x[1] = x0; y[1] = y0; z[1] = z0
+  for (i in 1:(steps - 1)) {
+    d = f(x[i], y[i], z[i])
+    x[i + 1] = x[i] + dt * d$dx
+    y[i + 1] = y[i] + dt * d$dy
+    z[i + 1] = z[i] + dt * d$dz
+  }
+  cbind(x, y, z)
+}
+
+# --- Lorenz Parameters from Science paper (rho = 28) ---
+fLorenz = function(x, y, z, sigma = 10, rho = 28, beta = 8 / 3) {
+  dx = sigma * (y - x)
+  dy = x * (rho - z) - y
+  dz = x * y - beta * z
+  list(dx = dx, dy = dy, dz = dz)
+}
+
+# --- Simulate Lorenz ---
+lorenz = simulate_attractor(fLorenz, 0, 0.1, 0, steps = 5000)
+cols = "#e6a922"  # Golden color
+
+# --- Plot M view (3D manifold) ---
+
+png("M.png", width = 1600, height = 1600, res = 300, bg = "transparent")
+plot3D::lines3D(lorenz[,1], lorenz[,2], lorenz[,3], colvar = NULL, col = cols,
+                theta = 10, phi = 0, pch = 19, lwd = 0.25, bty = "n", axes = FALSE)
+dev.off()
+
+# --- Plot MX view (Y–Z projection) ---
+
+png("Mx.png", width = 1600, height = 1600, res = 300, bg = "transparent")
+plot3D::lines3D(lorenz[,1], lorenz[,2], lorenz[,3], colvar = NULL, col = cols,
+                theta = 90, phi = 85, pch = 19, lwd = 0.25, bty = "n", axes = FALSE)
+dev.off()
+
+# --- Plot MY view (X–Z projection) ---
+
+png("My.png", width = 1600, height = 1600, res = 300, bg = "transparent")
+plot3D::lines3D(lorenz[,1], lorenz[,2], lorenz[,3], colvar = NULL, col = cols,
+                theta = 90, phi = -95, pch = 19, lwd = 0.25, bty = "n", axes = FALSE)
+dev.off()
